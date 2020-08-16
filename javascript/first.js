005a397b446f025480f02aefa7c6c6629f78740f
abrahamfasanya <abrahamimoleayo12@gmail.com>
2020-08-16 08:47:36 -0700

    initial commit

---
+++ javascript/first.js
@@ -0,0 +1,27 @@
+let products = [
+    {
+      name: "Rice",
+      description: "This is a description on rice",
+      stock: 560,
+      price: 1200,
+      sku: "1425i2234tjm423",
+      image: "https://www.royacshop.com/wp-content/uploads/2017/12/Ghana-rice.jpeg",
+      id: 1233520801,
+      showIfSoldOut: false,
+      isSoldOut: false,
+      owner: {
+        firstname: "Femi",
+        lastname: "Seun",
+        email: "gt@ht.com",
+        isEmailVerified: false,
+        companyName: "gateway",
+        isAdmin: false,
+        id: 1454512345010,
+        toothpaste : 57,
+        gala : 56,
+        biscuit : crackers,
+        class : 2020,
+      }
+    },
+  ]
+  
